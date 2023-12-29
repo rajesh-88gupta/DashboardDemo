bb47ed59b10ac3ef4a4840928374626e66a1f5e1
Rajesh Gupta <rajeshraj.gupta06@gmail.com>
2023-12-30 01:48:10 +0530

    small changes i forget it name of calling into main method

--- app/src/main/java/com/agro/dashboarddemo/MainActivity.kt
+++ app/src/main/java/com/agro/dashboarddemo/MainActivity.kt
@@ -62,7 +62,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           // DashBoard()
+            DashBoard()
         }
 
     }
